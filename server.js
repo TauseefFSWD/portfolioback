5ab232ffe5fcfb1b19083fef81028d0c55bbe579
Tauseef Akbar Ali Shaikh <mohdtauseef.ma32@gmail.com>
2024-08-01 17:59:39 +0530

    Added components as

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "https://portfr.onrender.com/", // Replace with your frontend URL
+  })
+);
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
